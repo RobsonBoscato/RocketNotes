64e3a6338305c9cd9950e178ecfc0a5217d8d707
Robson Boscato <rbsn.boscato@gmail.com>
2023-07-07 21:40:58 -0300

    started the project

---
+++ rocketnotes/src/components/Header/index.jsx
@@ -0,0 +1,25 @@
+import { RiShutDownLine } from "react-icons/ri";
+
+import { Container, Profile, Logout } from './styles'
+ 
+
+export function Header() {
+    return(
+        <Container>
+            <Profile>
+                <img src="https://github.com/robsonboscato.png"
+                alt="imagem do usuário" />
+
+                <div>
+                    <span>Bem-vindo,</span>
+                    <strong>Robson Boscato</strong>
+                </div>
+            </Profile>
+
+            <Logout>
+                <RiShutDownLine/>
+            </Logout>
+
+        </Container>
+    )
+}
---
+++ rocketnotes/src/pages/Details/index.jsx
@@ -0,0 +1,17 @@
+import { Container } from "./styles";
+
+import { Header } from "../../components/Header";
+import { Button } from "../../components/buttons";
+
+
+export function Details() {
+
+  return (
+  
+    <Container>
+     <Header/>
+
+      <Button title = "Back"/>
+    </Container>
+  )
+}
