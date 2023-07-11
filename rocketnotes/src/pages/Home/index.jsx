e964ff6f8362ddce497393c6feacb97510e9035a
Robson Boscato <rbsn.boscato@gmail.com>
2023-07-10 22:39:20 -0300

    added note component

--- rocketnotes/src/pages/Home/index.jsx
+++ rocketnotes/src/pages/Home/index.jsx
@@ -1,7 +1,12 @@
+import {FiPlus, FiSearch} from 'react-icons/fi';
+
 import {Container, Brand, Menu, Search, Content, NewNote} from "./styles";
 
+import { Note } from "../../components/Notes"
 import { Header } from "../../components/Header"
 import { ButtonText } from "../../components/ButtonText"
+import { Section } from "../../components/Section"
+import { Input } from "../../components/Input"
 
 export function Home(){
     return (
@@ -16,20 +21,32 @@ export function Home(){
 
             <Menu>
                 <li><ButtonText title="All" /></li>
-                <li><ButtonText title="React" /></li>
-                <li><ButtonText title="NodeJS" isActive/></li>
+                <li><ButtonText title="React" isActive/></li>
+                <li><ButtonText title="NodeJS" /></li>
 
             </Menu>
 
             <Search>
+                <Input icon={FiSearch} placeholder="Search Notes by Title" />
 
             </Search>
 
-            <Content>
-
+            <Content>                
+                <Section title="Minhas Notas">
+                    <Note data={{
+                        title: 'React', 
+                        tags: [
+                            {id: '1', name: 'React'},
+                            {id: '2', name: 'ExpressJS'}
+                        ]
+                        }} 
+                    />
+                </Section>
+               
             </Content>
 
             <NewNote>
+                <FiPlus/>Create Note
 
             </NewNote>
 
