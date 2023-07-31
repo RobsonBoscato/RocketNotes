38e83744d517eab89ea2e684210ea64fa738a53e
Robson Boscato <rbsn.boscato@gmail.com>
2023-07-31 10:01:00 -0300

    added axios library

--- rocketnotes/src/pages/SignUp/index.jsx
+++ rocketnotes/src/pages/SignUp/index.jsx
@@ -1,3 +1,7 @@
+import { useState } from 'react'
+
+import { api } from '../../services/api'
+
 import { Background, Container, Form } from './styles'
 import { Button } from '../../components/buttons'
 import { Input } from '../../components/Input'
@@ -6,6 +10,29 @@ import { Link } from 'react-router-dom'
 import {FiMail, FiLock, FiUser } from "react-icons/fi";
 
 export function SignUp(){
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+
+  function handleSignUp() {
+    if (!email || !email || !password) {
+      return alert("Please fill in all the required fields")
+    }
+
+    api.post("/users", {name, email, password })
+      .then(() => {
+        alert("User successfully registered")
+      })
+        .catch(error => {
+          if (error.response) {
+            alert(error.response.data.message)
+          }else {
+            alert("ERROR, can't register the user")
+          }
+        })
+    
+  }
+
   return (
     <Container>
       <Background/>
@@ -20,21 +47,24 @@ export function SignUp(){
         placeholder='Name'
         type='text'
         icon={FiUser}
+        onChange={e => setName(e.target.value)} 
         />
 
         <Input 
         placeholder='E-mail'
         type='text'
         icon={FiMail}
+        onChange={e => setEmail(e.target.value)}
         />
 
         <Input 
         placeholder='Password'
         type='password'
         icon={FiLock}
+        onChange={e => setPassword(e.target.value)}
         />
 
-        <Button title='Register'/>
+        <Button title='Register' onClick={handleSignUp}/>
 
         <Link to='/'>
           Return to login
